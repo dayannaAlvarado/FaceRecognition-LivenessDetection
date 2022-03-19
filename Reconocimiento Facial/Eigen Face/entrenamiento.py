42d6c7d9acdce6f3ce399a7744c9bdb4c8438db2
dayannaAlvarado <46323169+dayannaAlvarado@users.noreply.github.com>
2022-03-18 23:14:34 -0500

    Update entrenamiento.py

--- Reconocimiento Facial/Eigen Face/entrenamiento.py
+++ Reconocimiento Facial/Eigen Face/entrenamiento.py
@@ -17,17 +17,13 @@ for nameDir in peopleList:
 		facesData.append(cv2.imread(personPath+'/'+fileName,0))	
 	label = label + 1
 
-# Métodos para entrenar el reconocedor
-#face_recognizer = cv2.face.EigenFaceRecognizer_create()
-#face_recognizer = cv2.face.FisherFaceRecognizer_create()
-face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+#Métodos para entrenar el reconocedor
+face_recognizer = cv2.face.EigenFaceRecognizer_create()
 
 # Entrenando el reconocedor de rostros
 print("Entrenando...")
 face_recognizer.train(facesData, np.array(labels))
 
 # Almacenando el modelo obtenido
-#face_recognizer.write('modeloEigenFace.xml')
-#face_recognizer.write('modeloFisherFace.xml')
-face_recognizer.write('modeloLBPHFace.xml')
+face_recognizer.write('modeloEigenFace.xml')
 print("Modelo almacenado...")
